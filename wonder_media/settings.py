96d8f5c99e5d4f8ddac046f50a540931bd4afdd5
paul_odhiambo <paulodhiambo962@gmail.com>
2020-06-29 09:15:52 +0300

    added static files

--- wonder_media/settings.py
+++ wonder_media/settings.py
@@ -115,7 +115,11 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
-
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+# Extra places for collect static to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 STATIC_URL = '/static/'
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, "media")
